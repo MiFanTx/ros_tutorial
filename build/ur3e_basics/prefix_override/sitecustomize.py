8fe7ab8226fc05a4fc06f645eca13086e2263711
MiFanTx <mifantx@hotmail.com>
2026-02-12 18:00:58 +0800

    Remove colcon artifacts and add gitignore

--- build/ur3e_basics/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/yide/ros2_tutorial_ws/install/ur3e_basics'
--- build/ur_dashboard_msgs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yide/ros2_tutorial_ws/src/Universal_Robots_ROS2_Driver/ur_dashboard_msgs
-# Build directory: /home/yide/ros2_tutorial_ws/build/ur_dashboard_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("ur_dashboard_msgs__py")
